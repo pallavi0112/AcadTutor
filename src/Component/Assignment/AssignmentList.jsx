8fef0a0c0738609fce41f5b3ec9bc032c7371365
pallavi0112 <73059576+pallavi0112@users.noreply.github.com>
2023-05-25 11:28:45 +0000

    Update frontend_prod to output generated at 1a1dd7d

--- src/Component/Assignment/AssignmentList.jsx
+++ src/Component/Assignment/AssignmentList.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import axios from 'axios'
 import Assignment from './Assignment'
 
 const AssignmentList = () => {
+useEffect( ()=>{
+  const GetAssignment = async () => {
+    try {
+      const response = await axios.get(
+        'http://127.0.0.1:8000/content/get_teacher_assignment',
+      );
+      const courses = response.data
+      console.log(courses)
+      // courses.map((object, index) => {
+      //   const sub = object.subjects
+      //   setCoursesData((item) => [
+      //     ...item,
+          
+      //       object
+          
+      //   ]);
+      // });
+
+    } catch (err) {
+              console.error(err);
+              }
+  };
+  GetAssignment()
+},[])
   return (
     <div>
-        <Assignment/>
         <Assignment/>
         <Assignment/>
         <Assignment/>
